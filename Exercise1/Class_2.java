7d53ff162351253455f857dee98a39cc492db88e
AJ Leichner <leichneraj@s.dcsdk12.org>
2021-01-21 23:32:42 -0700

    Complete the exercise 1 tasks.

---
+++ Exercise1/Class_2.java
@@ -0,0 +1,31 @@
+//Inheritance: Exercise #1 Class_2
+public class Class_2 extends Class_1 {
+   private int z;
+
+   //x = 0, y = 0, z = 0
+   public Class_2() {
+       super();
+       z = 0;
+   }
+
+   //x = x1, y = y1, z = z1
+   public Class_2(int x1, int y1, int z1) {
+       super();
+       z = z1;
+   }
+
+   //output x, y, z
+   public void print() {
+      System.out.println(super.toString() + z + " ");
+   }
+
+   public String toString() {
+      return super.toString() + z + " ";
+   }
+
+   //x = x1, y = y1, z = z1
+   public void set(int x1, int y1, int z1) {
+      super.set(x1, y1);
+      z = z1;
+   }
+}
---
+++ Exercise1/Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+
+    public static void main(String[] args) {
+        Class_2 test = new Class_2(0,0,0);
+        System.out.println(test.toString());
+        test.print();
+        test.set(1, 2, 3);
+        System.out.println(test.toString());
+        test.print();
+    }
+
+}
