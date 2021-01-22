8df4622836a51446b5ddabba6add8f6fd2571976
AJ Leichner <leichneraj@s.dcsdk12.org>
2021-01-22 00:08:23 -0700

    Change creation of test for class_2 to the default constructor.

--- Exercise1/Tester.java
+++ Exercise1/Tester.java
@@ -1,7 +1,7 @@
 public class Tester {
 
     public static void main(String[] args) {
-        Class_2 test = new Class_2(0,0,0);
+        Class_2 test = new Class_2();
         System.out.println(test.toString());
         test.print();
         test.set(1, 2, 3);
